665a1349fb99a66b70c88e7f5d6d34988810ad90
Cameron Olszewski <me@cameronolszewski.com>
2018-08-14 16:54:45 -0700

    senior portfolio website

--- sidebar-location-content.php
+++ sidebar-location-content.php
@@ -6,9 +6,9 @@
 
 <!--
 <div class="eventData clear">
-  <p>FRESH<br> Tuesday, June 12th <br>5:30&ndash;8:30 pm. 
+  <p>FRESH<br> Wednesday, August 29th <br>5:30&ndash;8:30 pm. 
 <br>
-<a target="_blank" href="http://www.customblocksportland.com/">Custom Blocks South</a>
+<a target="_blank" href="https://whoisowenjones.com">whoisowenjones">Custom Blocks South</a>
 900-998 SE Main St<br>
 Portland, OR 97214<br><a target="_blank" href="https://www.google.com/maps/place/Custom+Blocks/@45.5135241,-122.6586718,17z/data=!3m1!4b1!4m5!3m4!1s0x5495a0a004bacfef:0x4df13bbbfd244f38!8m2!3d45.5135204!4d-122.6564778">(map)</a></p>
   <p>If you would like to speak to or collaborate with our PSUGD grads, please register through <a target="_blank" href="https://psugd2018winterfresh.eventbrite.com/">Eventbrite</a> to attend Fresh.</p>
@@ -26,13 +26,13 @@ Portland, OR 97214<br><a target="_blank" href="https://www.google.com/maps/place
     
     <div class="section when">
         <h2>Wednesday, August 29th</h2><p>5:30&ndash;8:30 pm.
-        <br><a target="_blank" href="https://www.eventbrite.com/e/fresh-portland-state-senior-portfolio-show-tickets-46379530424?aff=efbeventtix">RSVP</a> to attend Fresh.</p>
+        <br><a target="_blank" href="https://www.eventbrite.com/e/summer-fresh-portland-state-senior-portfolio-show-tickets-48859736788">RSVP</a> to attend Fresh.</p>
     </div>
     
     <div class="section where">
-        <h2><a target="_blank" href="http://www.customblocksportland.com/">Owen Jones</a></h2>
+        <h2><a target="_blank" href="https://whoisowenjones.com">Owen Jones</a></h2>
         <p>408 NW 5th Ave<br>Portland, OR 97209</p>
-        <a target="_blank" href="https://www.google.com/maps/place/Custom+Blocks/@45.5135241,-122.6586718,17z/data=!3m1!4b1!4m5!3m4!1s0x5495a0a004bacfef:0x4df13bbbfd244f38!8m2!3d45.5135204!4d-122.6564778">(map)</a>
+        <a target="_blank" href="https://www.google.com/maps/place/Owen+Jones+%26+Partners+LTD/@45.5261219,-122.6774493,17z/data=!3m1!4b1!4m5!3m4!1s0x54950a0065b81367:0x94a08ef8adf5b992!8m2!3d45.5261219!4d-122.6752606">(map)</a>
     </div>
     
     <div class="how">
